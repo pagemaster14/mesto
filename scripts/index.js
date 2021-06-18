442e877430f24202f713345ed3219dcbbb9dd50e
Lovchev Sergey <pagemaster@yandex.ru>
2021-06-18 18:15:11 +0300

    3.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,31 @@
 const popupOpenButtonElement = document.querySelector('.edit-button')
 const popupElement = document.querySelector('.popup')
 const popupCloseButtonElement = popupElement.querySelector('.popup-close')
+let profileName = document.querySelector('.profile-info__name')
+let profileJob = document.querySelector('.profile-info__bio')
+let formElement = document.querySelector('.form')
+let nameInput = document.querySelector('.form__text_type_name')
+let jobInput = document.querySelector('.form__text_type_bio')
 
-console.log(popupOpenButtonElement, popupElement, popupCloseButtonElement)
-
-const togglePopupVisibility = function() {
-    popupElement.classList.toggle('popup_opened')
+const openPopup = function() {
+    popupElement.classList.add('popup_opened')
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
-const closePopupByClickOnOverlay = function(event) {
-    console.log(event.target, event.currentTarget)
-    if (event.target !== event.currentTarget) {
-        return
-    }
-
-    togglePopupVisibility()
+const closePopup = function() {
+    popupElement.classList.remove('popup_opened')
 }
-
-popupOpenButtonElement.addEventListener('click', togglePopupVisibility)
-popupCloseButtonElement.addEventListener('click', togglePopupVisibility)
-popupElement.addEventListener('click', closePopupByClickOnOverlay)
-
-let profileName = document.querySelector('.profile-info__name')
-let profileJob = document.querySelector('.profile-info__bio')
-let formElement = document.querySelector('.input')
-let nameInput = document.querySelector('.input__text_name')
-let jobInput = document.querySelector('.input__text_bio')
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 
-    togglePopupVisibility()
+    closePopup()
 }
 
-
 formElement.addEventListener('submit', formSubmitHandler);
+popupOpenButtonElement.addEventListener('click', openPopup)
+popupCloseButtonElement.addEventListener('click', closePopup)
+popupElement.addEventListener('click', closePopupByClickOnOverlay)
